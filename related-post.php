81bd7e40a0ba3773dc46555b124ea905f54c9aed
tiepnguyen <tiepnguyen220194@gmail.com>
2020-12-29 01:30:18 +0700

    up file

---
+++ related-post.php
@@ -0,0 +1,35 @@
+<aside class="related-post">
+	<div class="caption-text">
+		<span class="text">
+			<h3>Tin liên quan</h3>
+		</span>
+	</div>
+
+	<?php
+	    global $post;
+	    $categories = get_the_category($post->ID);
+
+	    if ($categories) {
+		    $category_ids = array();
+		    foreach($categories as $individual_category) $category_ids[] = $individual_category->term_id;
+		    $args=array(
+			    'category__in' => $category_ids,
+			    'post__not_in' => array($post->ID),
+			    'posts_per_page'=> 4,
+			    'ignore_sticky_posts'=>1
+		    );
+		    $my_query = new wp_query( $args );
+
+		    if($query->have_posts()) : while ($query->have_posts() ) : $query->the_post(); ?>
+
+					    <div class="title">
+					      <a href="<?php the_permalink()?>" rel="bookmark" title="<?php the_title(); ?>">
+					      	<h4><?php the_title(); ?></h4>
+					      </a>
+					    </div>
+
+			<?php endwhile; wp_reset_postdata(); else: echo ''; endif;
+	    }
+	?>
+
+</aside>
---
+++ related-product.php
@@ -0,0 +1,40 @@
+<?php
+	//$glb_ctp_product biến toàn cục
+	//$glb_tax_product biến toàn cục
+	global $post;
+	$terms = get_the_terms( $post->ID , $glb_tax_product, 'string');
+	$term_ids = wp_list_pluck($terms,'term_id');
+	$query = new WP_Query( array(
+		'post_type' => $glb_ctp_product,
+		'tax_query' => array(
+			array(
+				'taxonomy' => $glb_tax_product,
+				'field' => 'id',
+				'terms' => $term_ids,
+				'operator'=> 'IN'
+			 )),
+		'posts_per_page' => 4,
+		'orderby' => 'date',
+		'post__not_in'=>array($post->ID)
+	 ) );
+?>
+
+<aside class="related-product">
+	<div class="caption-text">
+		<span class="text">
+			<h3>Sản phẩm liên quan</h3>
+		</span>
+	</div>
+
+	<?php
+		if($query->have_posts()) : while ($query->have_posts() ) : $query->the_post(); ?>
+
+			<div class="title">
+		      <a href="<?php the_permalink()?>" rel="bookmark" title="<?php the_title(); ?>">
+		      	<h4><?php the_title(); ?></h4>
+		      </a>
+		    </div>
+
+	<?php endwhile; wp_reset_postdata(); else: echo ''; endif; ?>
+
+</aside>
